ab95ecdf4c3209e9ef5c65a3ddcba566d928434c
ibkon <btinggo@gmail.com>
2019-08-06 15:35:04 +0800

    Add webMagic

--- src/main/java/com/tigxu/net/Crawler.java
+++ src/main/java/com/tigxu/net/Crawler.java
@@ -11,7 +11,16 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -39,13 +48,57 @@ public class Crawler {
         }
         return null;
 	}
-	
+	/**
+	 * web magic爬虫框架
+	 */
 	@Test
-	public void test() throws IOException{
-		Document	document	= Jsoup.connect("http://s.gxrc.com/sCareer?posType=5467").get();
+	public void wmc() {
+		System.out.println("wmc start");
+	}
+	public void testat() {
+		HttpGet		httpGet		= new HttpGet("http://www.jd.com");
+		httpGet.addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
+		httpGet.addHeader("Accept-Language","zh-CN,zh;q=0.9,en-GB;q=0.8,en;q=0.7");
+		httpGet.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
+		RequestConfig	config	= RequestConfig.custom().setSocketTimeout(3000).setConnectTimeout(3000).build();
+		httpGet.setConfig(config);
+		CloseableHttpClient	build	= HttpClientBuilder.create().build();
+		try {
+			CloseableHttpResponse	httpResponse	= build.execute(httpGet);
+			if(httpResponse.getStatusLine().getStatusCode()==200) {
+				HttpEntity	entity	= httpResponse.getEntity();
+				System.out.println(EntityUtils.toString(entity));
+			}
+			else {
+				System.err.println(httpResponse.getStatusLine().getStatusCode());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		
+		
+		
+		
+	}
+	
+	public void testr() throws IOException{
+		//类选择器
+		//Document	document	= Jsoup.connect("http://s.gxrc.com/sCareer?posType=5467").get();
 		//System.out.println(document.select("a").last().html());
-		System.out.println(document.select(".w1").last().text());
+		//System.out.println(document.select(".w1").last().text());
+		
+		String	regex	= "[a-zA-z]+://[^\\s]*";
+		String	url		= "http://s.gxrc.com/sCareer?posType=5467";
+		Pattern	pattern	= Pattern.compile(regex);
+		Matcher	matcher	= pattern.matcher(url);
+		if(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+		
+		/**/
+		
 	}
 	
 	public void dotxt() throws IOException{
