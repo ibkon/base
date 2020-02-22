41a5ad67852d393e1eca5476f93292813ff9dc24
ibkon <btinggo@gmail.com>
2020-02-22 21:28:33 +0800

    Update

--- src/main/java/top/yukino/base/Application.java
+++ src/main/java/top/yukino/base/Application.java
@@ -1,7 +1,40 @@
 package top.yukino.base;
 
+/***
+ * 启动调度
+ * 
+ * @author 13723
+ *
+ */
 public class Application {
-	public static void main(String args[]){
-		
+	public static void main(String args[]) throws Exception {
+		if (args.length == 0) {
+			help();
+			return;
+		}
+		switch (args[0]) {
+		case "--fec":
+			if (args.length != 5)
+				System.err.println("--fec参数错误,[--fec * * * *] 输入文件，输出路径,输入编码，输出编码");
+			else
+				top.yukino.base.tool.LangCode.FileCode(args[1], args[2], args[3], args[4]);
+			break;
+		default:
+			help();
+			break;
+		}
+	}
+
+	/**
+	 * 打印帮助信息
+	 */
+	public static void help() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Ibkon java bases\n");
+		builder.append("===>本程序为个练习编写，其功能较为杂乱。\n");
+		builder.append("===>基本使用方法，目前仅提供命令行使用，计划今后加入UI使用。\n");
+		builder.append("--fec * * * *	文件编码功能（输入文件，输出路径,输入编码，输出编码）。\n");
+
+		System.out.println(builder);
 	}
 }
--- src/main/java/top/yukino/base/tool/LangCode.java
+++ src/main/java/top/yukino/base/tool/LangCode.java
@@ -6,29 +6,21 @@ import java.io.*;
  * 文件编码转换
  */
 public class LangCode {
-    public static void FileCode(String in,String out,String inCode,String outCode) throws IOException{
-        BufferedReader  reader;
-        BufferedWriter  writer;
-        String          temp;
-        File            outFile = new File(out);
-        reader      = new BufferedReader(new InputStreamReader(new FileInputStream(new File(in)),inCode));
-        if(!outFile.exists()){
-            outFile.createNewFile();
-        }
-        writer      = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile),outCode));
-        while((temp=reader.readLine())!=null){
-            writer.write(temp);
-            writer.newLine();
-        }
-        writer.flush();
-        writer.close();
-        reader.close();
-    }
-    public static void gbk2utf8(String in,String out){
-        try {
-            FileCode(in,out,"GBK","UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void FileCode(String in, String out, String inCode, String outCode) throws IOException {
+		BufferedReader reader;
+		BufferedWriter writer;
+		String temp;
+		File outFile = new File(out);
+		reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(in)), inCode));
+		if (!outFile.exists()) {
+			outFile.createNewFile();
+		}
+		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile), outCode));
+		while ((temp = reader.readLine()) != null) {
+			writer.write(temp);
+			writer.newLine();
+		}
+		writer.close();
+		reader.close();
+	}
 }
